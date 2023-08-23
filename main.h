325c7d22040716ea24098bc5901fd9235dd6ed3b
any-One <oussamakouraich@gmail.com>
2023-08-23 18:42:39 +0100

    [FIX] handeling errors

--- main.h
+++ main.h
@@ -164,4 +164,17 @@ int get_len(int n);
 char *aux_itoa(int n);
 int _atoi(char *s);
 
+/* auxError1.c */
+char *strcat_cd(data_shell *, char *, char *, char *);
+char *error_get_cd(data_shell *datash);
+char *error_not_found(data_shell *datash);
+char *error_exit_shell(data_shell *datash);
+
+/* auxError2.c */
+char *error_get_alias(char **args);
+char *error_env(data_shell *datash);
+char *error_syntax(char **args);
+char *error_permission(char **args);
+char *error_path_126(data_shell *datash);
+
 #endif
