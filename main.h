c65910e2a3434b0fb7f310e6f2b3ed759630849e
any-One <oussamakouraich@gmail.com>
2023-08-23 16:43:53 +0100

    [Fix] prototypes & variables

--- main.h
+++ main.h
@@ -11,6 +11,9 @@
 #include <fcntl.h>
 #include <signal.h>
 
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 2
+#define TOK_DELIM " \t\r\n\a"
 
 char* _strcat(char* dest, const char* src);
 char* _strcpy(char* dest, char* src);
@@ -19,9 +22,10 @@ char* _strchr(char* s, char c);
 int _strspn(char* s, char* accept);
 char* _strdup(const char* s);
 int _strlen(const char* s);
-void _memcpy(void* newptr, void* ptr, unsigned int size);
+void _memcpy(void* newptr, const void* ptr, unsigned int size);
 void* _realloc(void* ptr, unsigned int old_size, unsigned int new_size);
-
+int cmp_chars(char str[], const char* delim);
+char* _strtok(char str[], const char* delim);
 
 
 #endif
